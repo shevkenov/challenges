1e7c2ea2004ac9c0ce9cf102c196a7f6068905e5
Shevkenov Andrey <shevkenov@gmail.com>
2021-04-29 13:53:19 +0300

    simple table created by react-table

---
+++ simple-react-table/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Table from './components/Table.jsx'
+function App() {
+  return (
+    <div className="App">
+      <Table />
+    </div>
+  );
+}
+
+export default App;
